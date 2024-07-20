bc06dbb466a0ca9abeed5f01ebbdc1e45618bc08
samuelncaetano <samueln.caetano42@gmail.com>
2024-07-20 17:09:25 -0300

    feat(json_repository): apply dataclass to json repository

--- src/infrastructure/database/json/json_repository.py
+++ src/infrastructure/database/json/json_repository.py
@@ -1,30 +1,34 @@
 import json
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any
 
 from src.application.services.user_schema import pydantic_to_user, user_to_pydantic
 
 
+@dataclass
 class JSONRepository:
-    def __init__(self, db_path: str, model: type):
-        self.db_path = Path(db_path)
-        self.model = model
+    db_path: str
+    model: type
+    _path: Path = field(init=False)
 
-        self.db_path.parent.mkdir(parents=True, exist_ok=True)
-        if not self.db_path.exists():
+    def __post_init__(self):
+        self._path = Path(self.db_path)
+        self._path.parent.mkdir(parents=True, exist_ok=True)
+        if not self._path.exists():
             self._initialize_db()
 
     def _initialize_db(self):
-        with open(self.db_path, "w") as f:  # pylint: disable = W1514, C0103
+        with open(self._path, "w") as f:  # pylint: disable = W1514, C0103
             json.dump([], f)
 
     def load_data(self):
-        with open(self.db_path, "r") as f:  # pylint: disable = W1514, C0103
+        with open(self._path, "r") as f:  # pylint: disable = W1514, C0103
             data = json.load(f)
             return [self.model(**item) for item in data]
 
     def save_data(self, data: Any):
-        with open(self.db_path, "w") as f:  # pylint: disable = W1514, C0103
+        with open(self._path, "w") as f:  # pylint: disable = W1514, C0103
             data = map(user_to_pydantic, data)
             json.dump([item.model_dump() for item in data], f)
 
