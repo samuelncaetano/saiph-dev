e2e608e75bdf79232959cdb74318d405b1b6489b
samuelncaetano <samueln.caetano42@gmail.com>
2024-07-20 16:19:26 -0300

    feat(user_repository): create the user repository

---
+++ src/infrastructure/database/json/user_repository.py
@@ -0,0 +1,7 @@
+from src.domain.entities.user import UserModel
+from src.infrastructure.database.json.json_repository import JSONRepository
+
+
+class UserRepository(JSONRepository):
+    def __init__(self, db_path: str):
+        super().__init__(db_path=db_path, model=UserModel)
