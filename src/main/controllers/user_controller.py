bd5afa25e55f06088e19d52eef8ca19b056fdf87
samuelncaetano <samueln.caetano42@gmail.com>
2024-07-21 14:05:17 -0300

    feat(user_controller): Add validation in controller

--- src/main/controllers/user_controller.py
+++ src/main/controllers/user_controller.py
@@ -1,3 +1,5 @@
+from pydantic import ValidationError  # pylint: disable = E0401  # type: ignore
+
 from src.application.use_cases.user_use_cases import UserUseCases
 from src.domain.entities.user import User
 
@@ -7,9 +9,12 @@ class UserController:
         self.user_use_cases = user_use_cases
 
     def create_user(self, user_data: User) -> User:
-        user = User(**user_data)
-        self.user_use_cases.create_user(user)
-        return user
+        try:
+            user = User(**user_data)
+            self.user_use_cases.create_user(user)
+            return user
+        except ValidationError as error:
+            raise ValueError(f"Invalid user data: {error}")  # pylint: disable = W0707
 
     def list_users(self) -> list[User]:
         return self.user_use_cases.list_users()
