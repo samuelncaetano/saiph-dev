033671cc4c64a8540b67accdc8b8351ed6d09833
samuelncaetano <samueln.caetano42@gmail.com>
2024-07-21 12:59:22 -0300

    refactor(server): refactor the http server code

---
+++ src/main/routes/config.py
@@ -0,0 +1,10 @@
+from src.application.use_cases.user_use_cases import UserUseCases
+from src.infrastructure.database.json.user_repository import UserRepository
+from src.main.controllers.user_controller import UserController
+
+
+def configure_dependencies():
+    user_repository = UserRepository(db_path="database/users.json")
+    user_use_cases = UserUseCases(repository=user_repository)
+    user_controller = UserController(user_use_cases=user_use_cases)
+    return user_controller
--- src/main/routes/server.py
+++ src/main/routes/server.py
@@ -2,13 +2,9 @@ import json
 from dataclasses import asdict
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-from src.application.use_cases.user_use_cases import UserUseCases
-from src.infrastructure.database.json.user_repository import UserRepository
-from src.main.controllers.user_controller import UserController
+from config import configure_dependencies
 
-user_repository = UserRepository(db_path="database/users.json")
-user_use_cases = UserUseCases(repository=user_repository)
-user_controller = UserController(user_use_cases=user_use_cases)
+user_controller = configure_dependencies()
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -20,19 +16,25 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):  # pylint: disable = C0103
         if self.path == "/users":
-            users = user_controller.list_users()
-            users_json = json.dumps([asdict(user) for user in users])
-            self._send_response(200, "application/json", users_json)
+            try:
+                users = user_controller.list_users()
+                users_json = json.dumps([asdict(user) for user in users])
+                self._send_response(200, "application/json", users_json)
+            except Exception as error:
+                self._send_response(500, "application/json", json.dumps({"error": str(error)}))
         else:
             self._send_response(404, "text/plain", "Not Found")
 
     def do_POST(self):  # pylint: disable = C0103
         if self.path == "/users":
-            content_length = int(self.headers["Content-Length"])
-            post_data = self.rfile.read(content_length).decode("utf-8")
-            user_data = json.loads(post_data)
-            user = user_controller.create_user(user_data)
-            self._send_response(201, "application/json", json.dumps(asdict(user)))
+            try:
+                content_length = int(self.headers["Content-Length"])
+                post_data = self.rfile.read(content_length).decode("utf-8")
+                user_data = json.loads(post_data)
+                user = user_controller.create_user(user_data)
+                self._send_response(201, "application/json", json.dumps(asdict(user)))
+            except Exception as error:
+                self._send_response(500, "application/json", json.dumps({"error": str(error)}))
         else:
             self._send_response(404, "text/plain", "Not Found")
 
