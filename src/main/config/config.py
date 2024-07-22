639fee96256f2bdec75712229ddcf274e9989af4
samuelncaetano <samueln.caetano42@gmail.com>
2024-07-22 19:59:48 -0300

    refactor(config): change user dependency configuration parameters

--- src/main/config/config.py
+++ src/main/config/config.py
@@ -3,7 +3,7 @@ from src.infrastructure.repositories.user_repository import UserRepository
 from src.main.controllers.user_controller import UserController
 
 
-def configure_user_dependencies(db_path: str = "database/users.json") -> UserController:
+def configure_user_dependencies(db_path: str) -> UserController:
     user_repository = UserRepository(db_path=db_path)
     user_use_cases = UserUseCases(repository=user_repository)
     user_controller = UserController(user_use_cases=user_use_cases)
