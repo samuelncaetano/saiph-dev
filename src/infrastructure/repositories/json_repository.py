4f72f8a262c8db7f624c11de3bb0e6e978332b58
samuelncaetano <samueln.caetano42@gmail.com>
2024-08-04 11:21:59 -0300

    perf(logs): add logs to json repository

--- src/infrastructure/repositories/json_repository.py
+++ src/infrastructure/repositories/json_repository.py
@@ -7,7 +7,7 @@ from tempfile import NamedTemporaryFile
 from typing import Any
 
 # Criar um logger
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("json_repository")
 
 
 @dataclass
@@ -18,32 +18,32 @@ class JSONRepository:
 
     def __post_init__(self):
         self._path = Path(self.db_path)
-        logging.debug(f"Initializing JSONRepository with path: {self._path}")
+        logger.debug(f"Initializing JSONRepository with path: {self._path}")
         self._path.parent.mkdir(parents=True, exist_ok=True)
         if not self._path.exists():
             self._initialize_db()
 
     def _initialize_db(self):
-        logging.debug(f"Creating new database file at: {self._path}")
+        logger.debug(f"Creating new database file at: {self._path}")
         with open(self._path, "w") as f:  # pylint: disable = W1514, C0103
             json.dump([], f)
-        logging.info(f"Database initialized at: {self._path}")
+        logger.info(f"Database initialized at: {self._path}")
 
     def load_data(self):
-        logging.debug(f"Loading data from: {self._path}")
+        logger.debug(f"Loading data from: {self._path}")
         with open(self._path, "r") as f:  # pylint: disable = W1514, C0103
             data = json.load(f)
-            # logging.info(f'Data loaded: {data}')
+            # logger.info(f'Data loaded: {data}')
             return [self.model(**item) for item in data]
 
     def save_data(self, data: Any):
-        logging.debug(f"Saving data to: {self._path}")
+        logger.debug(f"Saving data to: {self._path}")
         temp_file = NamedTemporaryFile("w", delete=False, dir=self._path.parent)  # pylint: disable = R1732
         try:
             json.dump([item.model_dump() for item in data], temp_file)
             temp_file.flush()
             shutil.move(temp_file.name, self._path)
-            logging.info(f"Data successfully saved to: {self._path}")
+            logger.info(f"Data successfully saved to: {self._path}")
         finally:
             temp_file.close()
-            logging.debug(f"Temporary file closed: {temp_file.name}")
+            logger.debug(f"Temporary file closed: {temp_file.name}")
