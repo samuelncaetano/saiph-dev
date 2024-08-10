5f96b57d0360f5c77c9b399b3d2c5e2138d4039d
samuelncaetano <samueln.caetano42@gmail.com>
2024-08-10 16:34:12 -0300

    feat(book_controller): create the missing methods in the controller

--- src/main/controllers/book_controller.py
+++ src/main/controllers/book_controller.py
@@ -1,8 +1,8 @@
 from typing import Any, List
 
-# from src.application.services.book_schema import pydantic_to_book, book_to_pydantic
+from src.application.services.book_schema import book_to_pydantic, pydantic_to_book
 from src.application.use_cases.book_use_cases import BookUseCases
-from src.domain.entities.book import Book
+from src.domain.entities.book import Book, BookModel
 
 
 class BookController:
@@ -21,3 +21,16 @@ class BookController:
 
     def get_by_user_id(self, user_id: int) -> List[dict[str, Any]]:
         return self.book_use_case.get_by_user_id(user_id)
+
+    def update_book(self, book_id: int, book_data: dict[str, Any]) -> dict[str, Any]:
+        book_dict = self.get_by_id(book_id)
+        book = pydantic_to_book(BookModel(**book_dict))
+        for key, value in book_data.items():
+            setattr(book, key, value)
+
+        book_dict_pydantic = book_to_pydantic(book).model_dump()
+        updated_book = self.book_use_case.update_book(book_dict_pydantic)  # type: ignore
+        return updated_book
+
+    def delete_book(self, book_id: int) -> List[dict[str, Any]]:
+        return self.book_use_case.delete_book(book_id)
