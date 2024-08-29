876d67d213f58a966d2825aca289526989505c49
samuelncaetano <samueln.caetano42@gmail.com>
2024-08-29 09:57:14 -0300

    feat(book): add default status of books

--- backend/domain/entities/book.py
+++ backend/domain/entities/book.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any
 
 from pydantic import BaseModel, field_validator  # pylint: disable = E0401 # type: ignore
@@ -9,7 +9,7 @@ class Book:
     id: int  # pylint: disable = C0103
     title: str
     user_id: int
-    status: bool
+    status: bool = field(default=False)
 
     def get_title(self):
         return self.title
@@ -25,16 +25,18 @@ class BookModel(BaseModel):  # type: ignore
     id: int  # pylint: disable = C0103
     title: str
     user_id: int
-    status: bool
+    status: bool = False
 
     @field_validator("title", mode="before")
-    def title_not_empty(cls, value: str, field: Any) -> str:
+    def title_not_empty(cls, value: str, field: Any) -> str:  # pylint: disable = W0621
         if len(value) < 3:
-            raise ValueError(f"O campo '{field.field_name}' deve ter pelo menos 3 caracteres")
+            raise ValueError(
+                f"O campo '{field.field_name}' deve ter pelo menos 3 caracteres"  # pylint: disable = W0621
+            )
         return value
 
     @field_validator("user_id", mode="before")
-    def user_id_not_empty(cls, value: int, field: Any) -> int:
+    def user_id_not_empty(cls, value: int, field: Any) -> int:  # pylint: disable = W0621
         if value <= 0:
-            raise ValueError(f"O campo '{field.field_name}' deve ser vazio")
+            raise ValueError(f"O campo '{field.field_name}' deve ser vazio")  # pylint: disable = W0621
         return value
